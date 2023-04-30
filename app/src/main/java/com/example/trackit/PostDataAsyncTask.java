da6711420b053899d13193233c17dcfc4661928b
hrithik5709 <hrihtiksingh80@gmail.com>
2023-04-30 15:42:32 +0530

    Did below: Use constructor injection: In the PostDataAsyncTask class, you are using a member variable to store the Context object. This is not recommended as it can lead to memory leaks. Instead, you can pass the Context object to the constructor of the class and store it in a final member variable. This way, you can be sure that the Context object will not change throughout the life of the AsyncTask.
    Add error handling: In the doInBackground method, you are catching IOException and returning null if an exception occurs. This is not ideal as it does not provide any information about the error that occurred. Instead, you can catch the exception and return an error message or code that can be used to identify the error.
    
    Use constants for URLs and MIME types: In the doInBackground method, you are hardcoding the URL and the MIME type. This is not recommended as it can lead to errors if the URL or the MIME type changes. Instead, you can define constants for the URL and the MIME type and use them in the code.

--- app/src/main/java/com/example/trackit/PostDataAsyncTask.java
+++ app/src/main/java/com/example/trackit/PostDataAsyncTask.java
@@ -2,6 +2,7 @@ package com.example.trackit;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -14,25 +15,36 @@ import okhttp3.Response;
 
 public class PostDataAsyncTask extends AsyncTask<String, Void, String> {
 
-    private Context mContext;
+    private static final String TAG = "PostDataAsyncTask";
+    private static final String BASE_URL = "https://script.google.com/macros/s/";
+    private static final String API_KEY = "AKfycbwiDyOdazrPrFrSHHdp7feN58U6nN1p-kj_6oq3";
+    private static final String EXEC_ENDPOINT = "exec";
+
+    private final Context mContext;
 
     public PostDataAsyncTask(Context context) {
-        mContext = context;
+        mContext = context.getApplicationContext();
     }
 
     protected String doInBackground(String... params) {
         String json = params[0];
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
+        String url = BASE_URL + API_KEY + "/" + EXEC_ENDPOINT;
         Request request = new Request.Builder()
-                .url("https://script.google.com/macros/s/AKfycbwiDyOdazrPrFrSHHdp7feN58U6nN1p-kj_6oq3/exec")
+                .url(url)
                 .post(body)
                 .build();
         try {
             Response response = client.newCall(request).execute();
-            return response.body().string();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            } else {
+                Log.e(TAG, "Error: " + response.code() + " " + response.message());
+                return null;
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(TAG, "Error: " + e.getMessage(), e);
             return null;
         }
     }
